a1b70a9f80468bffabd9adec4e1883b3accf1bdc
Remo Arpagaus <arpagaus@users.noreply.github.com>
2019-02-26 21:42:03 +0100

    Test with ReflectResponder

--- src/main/java/example/Main.java
+++ src/main/java/example/Main.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,63 +18,60 @@
 
 package example;
 
+import example.proto.IMailService;
+import example.proto.Message;
+import org.apache.avro.ipc.*;
+import org.apache.avro.ipc.reflect.ReflectRequestor;
+import org.apache.avro.ipc.reflect.ReflectResponder;
+
 import java.io.IOException;
 import java.net.InetSocketAddress;
-
-import org.apache.avro.ipc.NettyServer;
-import org.apache.avro.ipc.NettyTransceiver;
-import org.apache.avro.ipc.Server;
-import org.apache.avro.ipc.specific.SpecificRequestor;
-import org.apache.avro.ipc.specific.SpecificResponder;
-import org.apache.avro.util.Utf8;
-
-import example.proto.Mail;
-import example.proto.Message;
+import java.net.URL;
+import java.time.LocalDateTime;
+import java.util.Arrays;
 
 /**
  * Start a server, attach a client, and send a message.
  */
 public class Main {
-    public static class MailImpl implements Mail {
+    public static class MailServiceImpl implements IMailService {
         // in this simple example just return details of the message
-        public Utf8 send(Message message) {
-            System.out.println("Sending message");
-            return new Utf8("Sending message to " + message.getTo().toString()
-                    + " from " + message.getFrom().toString()
-                    + " with body " + message.getBody().toString());
+        public String send(Message message) {
+            System.out.println("### SERVER: Sending message");
+            return "Sending message to " + message.to
+                    + " from " + message.from
+                    + " with body " + message.body;
         }
     }
 
     private static Server server;
 
     private static void startServer() throws IOException {
-        server = new NettyServer(new SpecificResponder(Mail.class, new MailImpl()), new InetSocketAddress(65111));
-        // the server implements the Mail protocol (MailImpl)
+        server = new HttpServer(new ReflectResponder(IMailService.class, new MailServiceImpl()), 65111);
+        server.start();
+        // the server implements the IMailService protocol (MailServiceImpl)
     }
 
     public static void main(String[] args) throws IOException {
-        if (args.length != 3) {
-            System.out.println("Usage: <to> <from> <body>");
-            System.exit(1);
-        }
-
         System.out.println("Starting server");
         // usually this would be another app, but for simplicity
         startServer();
         System.out.println("Server started");
 
-        NettyTransceiver client = new NettyTransceiver(new InetSocketAddress(65111));
+        Transceiver client = new HttpTransceiver(new URL("http://localhost:8080"));
         // client code - attach to the server and send a message
-        Mail proxy = (Mail) SpecificRequestor.getClient(Mail.class, client);
+        IMailService proxy = ReflectRequestor.getClient(IMailService.class, client);
         System.out.println("Client built, got proxy");
 
         // fill in the Message record and send it
         Message message = new Message();
-        message.setTo(new Utf8(args[0]));
-        message.setFrom(new Utf8(args[1]));
-        message.setBody(new Utf8(args[2]));
+        message.to = "info@abc.com";
+        message.from = "remo@github.com";
+        message.body = "Hello abc!\nSome new text ...";
+        message.attachments = Arrays.asList(new Message.Attachment("first", 66),new Message.Attachment("second", 234));
+
         System.out.println("Calling proxy.send with message:  " + message.toString());
-        System.out.println("Result: " + proxy.send(message));
+        System.out.println("### CLIENT: Response: " + proxy.send(message));
 
         // cleanup
         client.close();
